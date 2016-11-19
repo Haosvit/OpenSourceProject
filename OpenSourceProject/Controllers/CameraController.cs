7245832d09c132c1b5bfb2ebe595a1bae2c566ff
Hao Svit <haonguyen.svit@gmail.com>
2016-11-19 13:32:04 +0700

    + Giao dien, camera

---
+++ OpenSourceProject/Controllers/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OpenSourceProject.Controllers
+{
+	public class CameraController : Controller
+	{
+		//
+		// GET: /Camera/
+		public ActionResult Index()
+		{
+			return PartialView("Index");
+		}
+	}
+}
---
+++ OpenSourceProject/Scripts/App/camera-handler.js
@@ -0,0 +1,25 @@
+﻿function UploadPicture() {
+	$.ajax({
+		type: 'POST',
+		url: ("@Url.Content("~/Login/UploadPicture")/"),
+		dataType: 'json',
+		success: function (data) {
+		}
+});
+}
+
+function Uploadsubmit() {
+	debugger;
+	var src = $('img').attr('src');
+	src_array = src.split('/');
+	src = src_array[4];
+	if (src != "") {
+		$.ajax({
+			type: 'POST',
+			url: ("@Url.Content("~/Photo/Index")/"),
+		dataType: 'json',
+		data: { Imagename: src },
+		success: function () { }
+	});
+	window.opener.location.href = "http://localhost:55694/Photo/Changephoto";
+	self.close();}}
